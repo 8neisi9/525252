ce378bf8ef0fab26766e86503eb3935e741e9838
8neisi9 <99281093+8neisi9@users.noreply.github.com>
2026-05-26 19:35:00 +0300

    Delete Шишебаров Артем ИСП-422А directory

--- Шишебаров Артем ИСП-422А/SQLQuery3.sql
+++
@@ -1,8 +0,0 @@
-use BsAll;
-GO
-open symmetric key UserSymmetricKey 
-decryption by certificate UserCertificate;
-update Users 
-set pswd = ENCRYPTBYKEY(KEY_GUID('UserSymmetricKey'), CONVERT(varbinary(MAX), pswd))
-close symmetric key UserSymmetricKey;
-go
--- Шишебаров Артем ИСП-422А/SQLQuery4.sql
+++
@@ -1,7 +0,0 @@
-use BsAll;
-go
-open symmetric key UserSymmetricKey decryption by certificate UserCertificate;
-select userID, pswd, CONVERT(varchar(50), DECRYPTBYKEY(convert(varbinary(max), pswd))) as decrypted_pswd
-from Users;
-close symmetric key UserSymmetricKey;
-go
