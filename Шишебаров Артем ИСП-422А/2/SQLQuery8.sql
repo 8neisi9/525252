79d672db6f93481c528145e58a234116fce3c36a
8neisi9 <99281093+8neisi9@users.noreply.github.com>
2026-05-26 18:46:59 +0300

    Delete Шишебаров Артем ИСП-422А directory

--- Шишебаров Артем ИСП-422А/2/SQLQuery8.sql
+++
@@ -1,8 +0,0 @@
-use BsAll;
-go
-create master key encryption by password = 'passwOrd123'
-open master key decryption by password = 'passwOrd123';
-create certificate UserCertificate with subject = 'UserCertificate';
-create symmetric key UserSymmetricKey with algorithm = aes_256 encryption by certificate UserCertificate;
-close master key;
-go
